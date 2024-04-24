580546a0696e3d907d33a23d64741bb19e2a0b25
Dipan-hub <97114647+Dipan-hub@users.noreply.github.com>
2024-04-24 17:26:44 +0530

    Create functionW_01.h

---
+++ KKS_FD_CUDA_MPI/functions/functionW_01.h
@@ -0,0 +1,33 @@
+#ifndef FUNCTIONW_01_CUH_
+#define FUNCTIONW_01_CUH_
+
+#include <stdio.h>
+#include <cmath>  // Replace CUDA-specific includes with standard C++ headers
+//#include <cstdio>
+
+#include "structures.h"
+#include "utilityFunctions.h"
+
+
+/*
+ * __device__ double calcDoubleObstacleDerivative
+ *
+ * Calculate g'(\phi)
+ *
+ * Arguments:
+ *              1. cufftDoubleComplex **phi -> all the phase volume-fraction values
+ *              2. long phase -> differentiate wrt to this phase
+ *              3. double *theta_i   -> coefficients for theta_i, one per phase
+ *              4. double *theta_ij  -> coefficients for theta_ij, one per pair of phases
+ *              5. double *theta_ijk -> coefficients for theta_ijk, one per triplet of phases
+ *              6. long idx -> position of cell in 1D
+ *              7. long NUMPHASES -> number of phases
+ * Return:
+ *              numerical evaluation of derivative of interpolation polynomial, as a double datatype
+ */
+#pragma acc routine seq
+double calcDoubleObstacleDerivative(double **phi, long phase,
+                                    double *theta_i, double *theta_ij, double *theta_ijk,
+                                    long idx, long NUMPHASES);
+
+#endif
